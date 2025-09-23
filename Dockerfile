575daebcbbc0a1352d9664c1b8b3b20e9e1687ab
cloud-network-guy <151690046+cloud-network-guy@users.noreply.github.com>
2025-09-23 16:31:35 -0700

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.13-alpine
+WORKDIR /tmp
+COPY ./pyproject.toml ./
+RUN pip install --upgrade pip && pip install . --break-system-packages
+ENV PORT=8080
+ENV WSGI_DIR=/opt
+ENV WSGI_APP=wsgi:application
+COPY wsgi.py $WSGI_DIR
+COPY main.py $WSGI_DIR
+ENTRYPOINT gunicorn -b 0.0.0.0:$PORT -w 3 --access-logfile '-' --chdir $WSGI_DIR $WSGI_APP
+EXPOSE $PORT/tcp
---
+++ wsgi.py
@@ -0,0 +1,32 @@
+import traceback
+import json
+
+def application(environ, start_response):
+
+    try:
+
+        # Create a standard WSGI response with HTTP 200 code
+        http_response = {
+            'statusCode': "200 OK",
+            'headers': [("Content-Type", "application/json")],
+            'body': json.dumps({"message": "Hello there"}),
+        }
+
+    except Exception as e:
+
+        # Return a 500 status code with the error message in plain text
+        http_response = {
+            'statusCode': "500 Internal Server Error",
+            'headers': [('Content-type', "text/plain")],
+            'body': traceback.format_exc(),
+        }
+
+    # Start the WSGI response with code and headers
+    start_response(http_response.get('statusCode', "000 Unknown") + " ", http_response.get('headers', []))
+
+    if body := http_response.get('body'):
+        # If response body is string, we must encode it to bytestring
+        return [body.encode('utf-8') if isinstance(body, str) else body]
+    
+    return []
+
